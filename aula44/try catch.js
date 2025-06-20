b8187a884c2b892bbf75c590271fe0d39cbf74dc
gabriel tavares <gsantostavares@gmail.com>
2025-06-20 13:42:03 -0300

    Try Catch Throw

---
+++ aula44/try catch.js
@@ -0,0 +1,21 @@
+//Throw, and Try...Catch...Finally
+//The try statement defines a code block to run (to try).
+//The catch statement defines a code block to handle any error.
+//The finally statement defines a code block to run regardless of the result.
+//The throw statement defines a custom error.
+
+function soma(x, y) {
+    if (
+        typeof x !== 'number' || typeof y !== 'number' 
+    ) {
+        throw('x e y precisam ser números.')
+    }
+    return x + y
+}
+
+try {
+    console.log(soma(1, 2));
+    console.log(soma('1', 2));
+} catch(error) {
+    console.log(error);
+}
