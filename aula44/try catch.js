3ead0d071ccd85a955a2d0e05569596614515d7b
gabriel tavares <gsantostavares@gmail.com>
2025-06-20 18:55:01 -0300

    Especificando o erro a ser tratado

--- aula44/try catch.js
+++ aula44/try catch.js
@@ -19,3 +19,20 @@ try {
 } catch(error) {
     console.log(error);
 }
+
+function soma(x, y) {
+    if (
+        typeof x !== 'number' || typeof y !== 'number' 
+    ) {
+        throw new SyntaxError('x e y precisam ser números.')
+    }
+    return x + y
+}
+
+try {
+    console.log(soma(1, 2));
+    console.log(soma('1', 2));
+} catch(error) {
+    console.log(error);
+}
+console.log(5+5)
